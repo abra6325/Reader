ad692b67986f63bb384e871f907419b156b3e425
BuildTools <unconfigured@null.spigotmc.org>
2024-05-18 08:54:03 +0800

    I make stuf

--- Analysis.py
+++ Analysis.py
@@ -1,25 +1,29 @@
 import tensorflow as tf
 from keras import Sequential,layers,Model
+import typing
 
+def res(inp,stride,reshape = False,padding = "same") -> tf.Tensor:
+    idd = inp
+    x = layers.Conv2D(1, 3,strides=stride, padding = padding)(inp)
+    x = layers.BatchNormalization()(x)
+    print(x.shape)
+    x = layers.Conv2D(1, 3, padding = padding)(x)
 
-def res(inp,stride,reshape = False):
-    x = layers.Conv2D(1, 3,strides=stride)(inp)
     x = layers.BatchNormalization()(x)
-    x = layers.Conv2D(1, 3,strides = stride)(x)
-    x = layers.BatchNormalization()(x)
-    if reshape: idd = layers.Reshape((inp.shape[-3]-4,inp.shape[-2]-4,1))(x)
-    else: idd=inp
+    if(stride > 1):
+        idd = layers.Conv2D(1,1,padding = padding, strides = stride)(idd)
     x = layers.Add()([idd, x])
     x = layers.ReLU()(x)
-    return x
 
+    return x
 
 # input 1024x1024 image
 # output 4
-inputs = layers.Input(shape=(1024, 1024,1))
-i2 = layers.Conv2D(1,3)(inputs)
-r1 = res(i2,1,reshape = True)
-r2 = res(r1,2)
-out1 = layers.Dense(1)(r2)
-model = Model(inputs,out1)
-model.summary()
+def create_model():
+    inputs = layers.Input(shape=(1024, 1024,1))
+    i2 = layers.Conv2D(1,3)(inputs)
+    r1 = res(i2,1)
+    r2 = res(r1,2)
+    out1 = layers.Dense(1)(r2)
+    model = Model(inputs,out1)
+    return model
