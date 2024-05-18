d04ac7e7ffb0a2c50ac28f6a75958f051ddd1ff3
BuildTools <unconfigured@null.spigotmc.org>
2024-05-18 08:00:47 +0800

    I make stuf

--- Analysis.py
+++ Analysis.py
@@ -1,1 +1,25 @@
-print("di")
+import tensorflow as tf
+from keras import Sequential,layers,Model
+
+
+def res(inp,stride,reshape = False):
+    x = layers.Conv2D(1, 3,strides=stride)(inp)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(1, 3,strides = stride)(x)
+    x = layers.BatchNormalization()(x)
+    if reshape: idd = layers.Reshape((inp.shape[-3]-4,inp.shape[-2]-4,1))(x)
+    else: idd=inp
+    x = layers.Add()([idd, x])
+    x = layers.ReLU()(x)
+    return x
+
+
+# input 1024x1024 image
+# output 4
+inputs = layers.Input(shape=(1024, 1024,1))
+i2 = layers.Conv2D(1,3)(inputs)
+r1 = res(i2,1,reshape = True)
+r2 = res(r1,2)
+out1 = layers.Dense(1)(r2)
+model = Model(inputs,out1)
+model.summary()
